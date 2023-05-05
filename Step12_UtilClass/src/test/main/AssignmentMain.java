577b8c0dd2c4fbaf40cb5c57113432c9967ef0b0
ar2723 <ar2723@naver.com>
2023-05-05 12:02:07 +0900

    23.05.05_주말숙제 마무리

--- Step12_UtilClass/src/test/main/AssignmentMain.java
+++ Step12_UtilClass/src/test/main/AssignmentMain.java
@@ -2,9 +2,7 @@ package test.main;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-
 import test.mypac.Lotto;
 
 /*
@@ -24,19 +22,26 @@ import test.mypac.Lotto;
  */
 public class AssignmentMain {
 	public static void main(String[] args) {
-		List<Lotto> lottoList = new ArrayList<>();
-		
 		Scanner scan = new Scanner(System.in);
-		
 		System.out.print("로또를 몇개 구입하시겠습니까? : ");
 		int num = scan.nextInt();
+		System.out.println("");
+		System.out.println("생성된 로또 번호는 다음과 같습니다");
+		System.out.println("");
 		
-		Lotto lotto = new Lotto();
-		
-		ArrayList<Integer> num1 = lotto.lottoNum();
-		//오름차순 정렬
-		Collections.sort(num1);
-		
-		System.out.println(num1);
-			}
+		//구입한 로또 숫자만큼 반복문 수행
+		for(int i = 0; i < num; i++) {
+			//Lotto 객체를 생성 후 참조값을 지역변수 lotto에 담기
+			Lotto lotto = new Lotto();
+			//lotto 지역 변수에 담긴 객체의 참조값을 이용해서 lottoNum() 메소드를 호출 후 
+			//배열의 반환 값을 지역 변수에 담기
+			ArrayList<Integer> num1 = lotto.lottoNum();
+			//생성된 로또 번호 배열을 오름차순으로 정렬
+			Collections.sort(num1);
+			//정렬된 로또 번호 배열을 출력
+			System.out.println(i+1+"회차");
+			System.out.println(num1);
+			System.out.println("");
+		}
 	}
+}
--- Step12_UtilClass/src/test/mypac/Lotto.java
+++ Step12_UtilClass/src/test/mypac/Lotto.java
@@ -1,17 +1,15 @@
 package test.mypac;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class Lotto {
 	public ArrayList<Integer> lottoNum () {
-		ArrayList<Integer> numList = new ArrayList<>();
-		for(int i = 0; i <= 6; i++) {
-			numList.add((int)(Math.random()*50));
-		}
-		
-		
-
-		
-		return numList;
+		Set<Integer> numSet = new HashSet<>();
+	    while(numSet.size() < 6) {
+	        numSet.add((int)(Math.random()*50) + 1);
+	    }
+	    return new ArrayList<>(numSet);
 	}
 }
