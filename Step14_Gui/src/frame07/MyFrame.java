dd5de354a94655929f7b8231e8837c883f8835cd
ar2723 <ar2723@naver.com>
2023-05-10 12:28:32 +0900

    Step14 07frame 추가 및 06 수정

--- Step14_Gui/src/frame07/MyFrame.java
+++ Step14_Gui/src/frame07/MyFrame.java
@@ -1,4 +1,4 @@
-package frame06;
+package frame07;
 
 import java.awt.Component;
 import java.awt.FlowLayout;
@@ -11,25 +11,25 @@ import javax.swing.JOptionPane;
 
 //자바의 GUI는 그렇게 잘 쓰이지는 않는다.
 public class MyFrame extends JFrame implements ActionListener{
-	//필드
-	JButton sendBtn, deleteBtn, updateBtn; // 동일한 type인 경우 1줄에 선언할 수 있다.
 	
 	//ActionListener 인터페이스 구현에 따른 메소드 오버라이드
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//메소드의 매개변수로 전달되는 ActionEvent 객체에 이벤트에 대한 정보가 들어 있다.
-		//이 객체를 활용하면 어떤 버튼이 눌러졌는지 구별할 수 있다.
-		//눌러진 버튼의 참조값 얻어오기
-		Object which = e.getSource();
-		
-		if(which == sendBtn) {
-			//전송버튼을 누른 것이다.
+		//눌러진 버튼의 action command 읽어오기
+		String cmd = e.getActionCommand();
+		/*
+		 * java에서 문자열의 참조값이 같은지 비교 할때는 비교 연산자 == 이용해서 비교 하면 되지만
+		 * 문자열의 참조값이 아닌 내용이 같은지 비교할 때는 String의 .equals() 메소드를 이용해서 비교해야 한다.
+		 * 
+		 * 왜냐하면 java에서는 문자열을 만드는 방법에 따라서 내용은 같지만 참조값이 다르게 나오는 경우도 있기 때문이다.
+		 * 따라서 문자열은 내용이 같은지 정확히 비교할 때는 반드시 .equals() 메소드를 이용해야 한다.
+		 * (이걸 까먹으면 밤 셀수도 있음)
+		 */
+		if(cmd.equals("send")) {
 			JOptionPane.showMessageDialog(this, "전송합니다.");
-		} else if (which == deleteBtn){
-			//삭제버튼을 누른 것이다.
+		} else if(cmd.equals("delete")) {
 			JOptionPane.showMessageDialog(this, "삭제합니다.");
-		} else if (which == updateBtn){
-			//수정버튼을 누른 것이다.
+		} else if(cmd.equals("update")) {
 			JOptionPane.showMessageDialog(this, "수정합니다.");
 		}
 	}
@@ -48,13 +48,13 @@ public class MyFrame extends JFrame implements ActionListener{
 		setLayout(new FlowLayout());
 		
 		//전송 버튼의 참조값을 필드에 저장 (this. 은 생략 가능)
-		sendBtn = new JButton("전송");
+		JButton sendBtn = new JButton("전송");
 		add(sendBtn);
 		
-		deleteBtn = new JButton("삭제");
+		JButton deleteBtn = new JButton("삭제");
 		add(deleteBtn);
 	
-		updateBtn = new JButton("수정");
+		JButton updateBtn = new JButton("수정");
 		add(updateBtn);
 		
 		//전송, 삭제, 수정 버튼 모두 다 하나의 리스너 등록하기
@@ -62,11 +62,15 @@ public class MyFrame extends JFrame implements ActionListener{
 		deleteBtn.addActionListener(this);
 		updateBtn.addActionListener(this);
 		
+		//각각의 버튼에 action 명령을 설정할 수 있다.
+		sendBtn.setActionCommand("send");
+		deleteBtn.setActionCommand("delete");
+		updateBtn.setActionCommand("update");
 		
 		// 프레임을 화면상에 실제 보이게 하기 (false하면 화면에 보이지 않는다)
 		setVisible(true);
-		
 	}
+	
 	public static void main(String[] args) {
 		//MyFrame 객체 생성
 		new MyFrame("나의 프레임");
